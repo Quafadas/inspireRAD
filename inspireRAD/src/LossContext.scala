5bf213305135842b9221148904389b9956a60e91
Simon Parten <quafadas@gmail.com>
2025-09-15 15:06:06 +0200

    .

--- inspireRAD/src/LossContext.scala
+++ inspireRAD/src/LossContext.scala
@@ -3,6 +3,57 @@ package io.github.quafadas.inspireRAD
 import scala.reflect.ClassTag
 import narr.NArray
 import vecxt.matrix.Matrix
+import cats.Show
+import vecxt.all.row
+
+trait ShowContext[T, Matrix[T], Array[T], Scalar[T]]:
+  given shM: cats.Show[Matrix[T]]
+  given shA: cats.Show[Array[T]]
+  given shS: cats.Show[Scalar[T]]
+end ShowContext
+
+object DetailShow extends ShowContext[Double, Matrix, NArray, Scalar]:
+
+  override given shM: Show[Matrix[Double]] =  new Show[Matrix[Double]] {
+    def show(mat: Matrix[Double]): String =
+      val rows = for i <- 0 until Math.min(1, mat.rows)
+              yield mat
+                .row(i)
+                .map(s => "%.3f".format(s).reverse.padTo(6, ' ').reverse)
+                .mkString(" | ")
+            val footer = ("-" * (rows.head.length))
+      val detail = (rows :+ footer).mkString("\n")
+      s"Matrix (${mat.shape})\n $detail"
+  }
+
+  override given shA: Show[Array[Double]] =  new Show[Array[Double]] {
+    def show(arr: Array[Double]): String =
+      arr.map(v => "%.3f".format(v)).mkString("[", ", ", "]")
+  }
+
+  override given shS: Show[Scalar[Double]] =  new Show[Scalar[Double]] {
+    def show(scalar: Scalar[Double]): String =
+      "%.3f".format(scalar.scalar)
+  }
+
+object LiteShow extends ShowContext[Double, Matrix, NArray, Scalar]:
+
+  override given shM: Show[Matrix[Double]] =  new Show[Matrix[Double]] {
+    def show(mat: Matrix[Double]): String =
+      s"Matrix (${mat.shape})"
+  }
+
+  override given shA: Show[Array[Double]] =  new Show[Array[Double]] {
+    def show(arr: Array[Double]): String =
+      s"Array (${arr.length})"
+  }
+
+  override given shS: Show[Scalar[Double]] =  new Show[Scalar[Double]] {
+    def show(scalar: Scalar[Double]): String =
+      "%.3f".format(scalar.scalar)
+  }
+
+
 
 trait LossContext[Matrix[_], Array[_], Scalar[_], T]:
   given mOps: Matrixy[Matrix, T]
