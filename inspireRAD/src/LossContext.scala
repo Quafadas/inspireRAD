b1ca42392b0132fa821fab3617c77bbec0ed8e5b
Simon Parten <quafadas@gmail.com>
2025-09-15 15:07:55 +0200

    .

--- inspireRAD/src/LossContext.scala
+++ inspireRAD/src/LossContext.scala
@@ -14,46 +14,42 @@ end ShowContext
 
 object DetailShow extends ShowContext[Double, Matrix, NArray, Scalar]:
 
-  override given shM: Show[Matrix[Double]] =  new Show[Matrix[Double]] {
+  override given shM: Show[Matrix[Double]] = new Show[Matrix[Double]]:
     def show(mat: Matrix[Double]): String =
-      val rows = for i <- 0 until Math.min(1, mat.rows)
-              yield mat
-                .row(i)
-                .map(s => "%.3f".format(s).reverse.padTo(6, ' ').reverse)
-                .mkString(" | ")
-            val footer = ("-" * (rows.head.length))
+      val rows =
+        for i <- 0 until Math.min(1, mat.rows)
+        yield mat
+          .row(i)
+          .map(s => "%.3f".format(s).reverse.padTo(6, ' ').reverse)
+          .mkString(" | ")
+      val footer = ("-" * (rows.head.length))
       val detail = (rows :+ footer).mkString("\n")
       s"Matrix (${mat.shape})\n $detail"
-  }
+    end show
 
-  override given shA: Show[Array[Double]] =  new Show[Array[Double]] {
+  override given shA: Show[Array[Double]] = new Show[Array[Double]]:
     def show(arr: Array[Double]): String =
       arr.map(v => "%.3f".format(v)).mkString("[", ", ", "]")
-  }
 
-  override given shS: Show[Scalar[Double]] =  new Show[Scalar[Double]] {
+  override given shS: Show[Scalar[Double]] = new Show[Scalar[Double]]:
     def show(scalar: Scalar[Double]): String =
       "%.3f".format(scalar.scalar)
-  }
+end DetailShow
 
 object LiteShow extends ShowContext[Double, Matrix, NArray, Scalar]:
 
-  override given shM: Show[Matrix[Double]] =  new Show[Matrix[Double]] {
+  override given shM: Show[Matrix[Double]] = new Show[Matrix[Double]]:
     def show(mat: Matrix[Double]): String =
       s"Matrix (${mat.shape})"
-  }
 
-  override given shA: Show[Array[Double]] =  new Show[Array[Double]] {
+  override given shA: Show[Array[Double]] = new Show[Array[Double]]:
     def show(arr: Array[Double]): String =
       s"Array (${arr.length})"
-  }
 
-  override given shS: Show[Scalar[Double]] =  new Show[Scalar[Double]] {
+  override given shS: Show[Scalar[Double]] = new Show[Scalar[Double]]:
     def show(scalar: Scalar[Double]): String =
       "%.3f".format(scalar.scalar)
-  }
-
-
+end LiteShow
 
 trait LossContext[Matrix[_], Array[_], Scalar[_], T]:
   given mOps: Matrixy[Matrix, T]
